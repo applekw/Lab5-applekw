84c3f70a1e7905234bd30592ebdf5921c062218a
Kyle Apple <applekw@rose-hulman.edu>
2011-03-24 16:14:54 -0400

    fourth test passed"

--- src/Prime.java
+++ src/Prime.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 
-
 /**
  * TODO Put here a description of what this class does.
- *
- * @author applekw.
- *         Created Mar 24, 2011.
+ * 
+ * @author applekw. Created Mar 24, 2011.
  */
 public class Prime {
 	public static ArrayList<Integer> factorization(int n) {
-		
+
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		
-		if (n==2) ret.add(2);
-		if (n==3) ret.add(3);
+
+		while (n % 2 == 0) {
+			ret.add(2);
+			n/=2;
+		}
+		if (n == 3)
+			ret.add(3);
 		return ret;
 	}
 }
