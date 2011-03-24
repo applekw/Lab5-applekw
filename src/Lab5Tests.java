16db3811357489b8ab308b02ea78465381eb29f2
Kyle Apple <applekw@rose-hulman.edu>
2011-03-24 15:59:54 -0400

    committing first test case

---
+++ src/Lab5Tests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author applekw.
+ *         Created Mar 24, 2011.
+ */
+public class Lab5Tests {
+
+	@Test
+	public void testOne(){
+		AssertEqual(new ArrayList<Integer>(), Prime.factorization(1));
+	}
+}
---
+++ src/Prime.java
@@ -0,0 +1,10 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author applekw.
+ *         Created Mar 24, 2011.
+ */
+public class Prime {
+
+}
